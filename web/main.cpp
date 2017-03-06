76c4fafd44e9df2f1d57eafcfc4448ab9d9a41b1
maqingqing <751747880@qq.com>
2017-03-06 09:35:57 +0800

    finish

--- web/main.cpp
+++ web/main.cpp
@@ -16,6 +16,12 @@ Copyright (c) 2017-2018 Dezhou Shen, Sogou Inc.
 
 #include <string.h>
 #include <signal.h>
+#include <mcheck.h>
+#include <iconv.h>
+#include <netinet/tcp.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <memory>
 
 #include "ace/OS_NS_string.h"
 #include "ace/OS_NS_sys_stat.h"
@@ -25,14 +31,70 @@ Copyright (c) 2017-2018 Dezhou Shen, Sogou Inc.
 #include "ace/SOCK_Stream.h"
 #include "ace/Auto_Ptr.h"
 
-char* getMimeType(char* fileName)
-{
-    char* extPos = strrchr(fileName,'.');
-    char* extension;
-    char* mimeType = "text/plain, charset=us-ascii";
+static const int no_delay = 1;
+static const long PAGESIZE = 4096;
+/* Converts a hex character to its integer value */
+inline char from_hex(char ch) {
+  return isdigit(ch) ? ch - '0' : tolower(ch) - 'a' + 10;
+}
 
-    if(extPos == NULL){
-        extension = "";
+/* Converts an integer value to its hex character*/
+inline char to_hex(char code) {
+  static char hex[] = "0123456789abcdef";
+  return hex[code & 15];
+}
+
+/* Returns a url-encoded version of str */
+/* IMPORTANT: be sure to free() the returned string after use */
+inline char *url_encode(char *str) {
+  mtrace();
+  char *pstr = str, *buf = reinterpret_cast<char*>(malloc(strlen(str) * 3 + 1)), *pbuf = buf;
+  while (*pstr) {
+    if (isalnum(*pstr) || *pstr == '-' || *pstr == '_' || *pstr == '.' || *pstr == '~') 
+      *pbuf++ = *pstr;
+    else if (*pstr == ' ') 
+      *pbuf++ = '+';
+    else 
+      *pbuf++ = '%', *pbuf++ = to_hex(*pstr >> 4), *pbuf++ = to_hex(*pstr & 15);
+    pstr++;
+  }
+  *pbuf = '\0';
+  mtrace();
+  return buf;
+}
+
+/* Returns a url-decoded version of str */
+/* IMPORTANT: be sure to free() the returned string after use */
+inline char *url_decode(char *str) {
+  mtrace();
+  char *pstr = str, *buf = reinterpret_cast<char*>(malloc(strlen(str) + 1)), *pbuf = buf;
+  while (*pstr) {
+    if (*pstr == '%') {
+      if (pstr[1] && pstr[2]) {
+        *pbuf++ = from_hex(pstr[1]) << 4 | from_hex(pstr[2]);
+        pstr += 2;
+      }
+    } else if (*pstr == '+') { 
+      *pbuf++ = ' ';
+    } else {
+      *pbuf++ = *pstr;
+    }
+    pstr++;
+  }
+  *pbuf = '\0';
+  mtrace();
+  return buf;
+}
+
+const char* getMimeType(const char* fileName)
+{
+    mtrace();
+    const char* extPos = strrchr(fileName,'.');
+    const char* extension;
+    const char* mimeType = "text/plain, charset=us-ascii";
+
+    if (extPos == NULL){
+        extension =  "";
     }else{
         extension = extPos + 1;
     }
@@ -40,95 +102,103 @@ char* getMimeType(char* fileName)
     /* Compare and return mimetype */
     switch(extension[0]){
         case 'b':
-            if(extension == "bmp")
-                mimeType = "image/bmp";
-            if(extension == "bin")
-                mimeType = "application/octet-stream";
+            if (strncmp(extension, "bmp", 3) == 0)
+                mimeType =  "image/bmp";
+            if (strncmp(extension, "bin", 3) == 0)
+                mimeType =  "application/octet-stream";
 
             break;
         case 'c':
-            if(extension == "csh")
-                mimeType = "application/csh";
-            if(extension == "css")
-                mimeType = "text/css";
+            if (strncmp(extension, "csh", 3) == 0)
+                mimeType =  "application/csh";
+            if (strncmp(extension, "css", 3) == 0)
+                mimeType =  "text/css";
 
             break;
         case 'd':
-            if(extension == "doc")
-                mimeType = "application/msword";
-            if(extension == "dtd")
-                mimeType = "application/xml-dtd";
+            if (strncmp(extension, "doc", 3) == 0)
+                mimeType =  "application/msword";
+            if (strncmp(extension, "dtd", 3) == 0)
+                mimeType =  "application/xml-dtd";
             break;
         case 'e':
-            if(extension == "exe")
-                mimeType = "application/octet-stream";
+            if (strncmp(extension, "exe", 3) == 0)
+                mimeType =  "application/octet-stream";
             break;
         case 'h':
-            if(extension == "html" || extension == "htm")
-                mimeType = "text/html";
+            if (strncmp(extension, "html", 4) == 0 || strncmp(extension, "htm", 3) == 0)
+                mimeType =  "text/html";
             break;
         case 'i':
-            if(extension == "ico")
-                mimeType = "image/x-icon";
+            if (strncmp(extension, "ico", 3) == 0)
+                mimeType =  "image/x-icon";
             break;
         case 'g':
-            if(extension == "gif")
-                mimeType = "image/gif";
+            if (strncmp(extension, "gif", 3) == 0)
+                mimeType =  "image/gif";
             break;
         case 'j':
-            if(extension == "jpeg" || extension == "jpg")
-                mimeType = "image/jpeg";
+            if (strncmp(extension, "jpeg", 4) == 0 || strncmp(extension, "jpg", 3) == 0)
+                mimeType =  "image/jpeg";
             break;
         case 'l':
-            if(extension == "latex")
-                mimeType = "application/x-latex";
+            if (strncmp(extension, "latex", 5) == 0)
+                mimeType =  "application/x-latex";
             break;
         case 'p':
-            if(extension == "png")
-                mimeType = "image/png";
-            if(extension == "pgm")
-                mimeType = "image/x-portable-graymap";
+            if (strncmp(extension, "png", 3) == 0)
+                mimeType =  "image/png";
+            if (strncmp(extension, "pgm", 3) == 0)
+                mimeType =  "image/x-portable-graymap";
             break;  
         case 'r':
-            if(extension == "rtf")
-                mimeType  = "text/rtf";
+            if (strncmp(extension, "rtf", 3) == 0)
+                mimeType  =  "text/rtf";
             break;
         case 's':
-            if(extension == "svg")
-                mimeType = "image/svg+xml";
-            if(extension == "sh")
-                mimeType = "application/x-sh";
+            if (strncmp(extension, "svg", 3) == 0)
+                mimeType =  "image/svg+xml";
+            if (strncmp(extension, "sh", 2) == 0)
+                mimeType =  "application/x-sh";
             break;
         case 't':
-            if(extension == "tar")
-                mimeType = "application/x-tar";
-            if(extension == "tex")
-                mimeType = "application/x-tex";
-            if(extension == "tif" || extension == "tiff")
-                mimeType = "image/tiff";
-            if(extension == "txt")
-                mimeType = "text/plain";
+            if (strncmp(extension, "tar", 3) == 0)
+                mimeType =  "application/x-tar";
+            if (strncmp(extension, "tex", 3) == 0)
+                mimeType =  "application/x-tex";
+            if (strncmp(extension, "tif", 3) == 0 || strncmp(extension, "tiff", 4) == 0)
+                mimeType =  "image/tiff";
+            if (strncmp(extension, "txt", 3) == 0)
+                mimeType =  "text/plain";
+            break;
+        case 'u':
+            if (strncmp(extension, "utf8", 4) == 0)
+                mimeType =  "text/plain, charset=utf8";
             break;
         case 'x':
-            if(extension == "xml")
-                mimeType = "application/xml";
+            if (strncmp(extension, "xml", 3) == 0)
+                mimeType =  "application/xml";
             break;
         default:
             break;
     }
-
+    mtrace();
     return mimeType;
 }
 
+#ifndef BOOST_AUTO_TEST_MAIN
 int
 ACE_TMAIN (int argc, ACE_TCHAR *argv[]) {
+    mtrace();
+    signal(SIGPIPE, SIG_IGN);
+
     ACE_INET_Addr server_addr;
     ACE_SOCK_Acceptor acceptor;
     ACE_SOCK_Stream peer;
     int buffer_size = 2048;
     
     int port = 80;
-    if(argc>1) {
+    if (argc>1) {
         ACE::write_n(ACE_STDOUT,argv[1],strlen(argv[1]));
         port = atoi(argv[1]);
     }
@@ -137,18 +207,22 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[]) {
         return 1;
     }
     if (acceptor.open(server_addr) == -1) {
-        ACE::write_n(ACE_STDOUT,"Fail to open server_addr",21);
-        return 1;
+        ACE::write_n(ACE_STDOUT,"Fail to open server_addr",24);
+        goto acceptor_error;
     }
     for (;;) {
         ACE::write_n(ACE_STDOUT,"waiting to connect",18);
         if (acceptor.accept(peer) == -1)
-            return 1;
+            goto acceptor_error;
+        // strace shows next line is of no use.
         peer.disable(ACE_NONBLOCK);
+        // disable Nagle's Algorithm (Ste93).
+        peer.set_option(ACE_IPPROTO_TCP,TCP_NODELAY,
+                        (void*)&no_delay,sizeof(int));
         ACE_Message_Block mb (buffer_size);
         char *head = mb.rd_ptr();
         for (;;) {
-            ACE::write_n(ACE_STDOUT,"waiting to recv",15);
+            // ACE::write_n(ACE_STDOUT,"waiting to recv",15);
             // recv data
             int result = peer.recv (mb.wr_ptr (),
                                     buffer_size-(mb.wr_ptr ()-mb.rd_ptr ()));
@@ -163,30 +237,37 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[]) {
                         *ptr = '\0';
                         
                         char* filename = 0;
-                        //remove /
+                        // remove / to make file under current folder
                         filename = ACE_OS::strdup (head + 1);
-                        auto_ptr<char> auto_filename (filename);
+                        std::unique_ptr<char> auto_filename (filename);
                         ACE::write_n(ACE_STDOUT, (const char *)filename, ptr - head - 1);
                         ACE::write_n(ACE_STDOUT, "\n", 1);
-                       
+                        char* dec_filename = url_decode(filename);
+                        std::unique_ptr<char> auto_url_decode (dec_filename);
+                        char* sec_dec_filename = url_decode(dec_filename);
+                        std::unique_ptr<char> auto_sec_dec_filename (sec_dec_filename);
+                        ACE::write_n(ACE_STDOUT, (const char *)sec_dec_filename, strlen(sec_dec_filename));
+                        filename = sec_dec_filename;
+                        
                         // Is this a query?
-                        if(strchr(filename,'?')!=NULL) {
+                        if (strchr(filename,'?') != NULL) {
                             ACE::write_n(ACE_STDOUT, "it is a query", 13);
                             ACE::write_n(ACE_STDOUT, (const char *)filename, strlen(filename));
+                            
                             // append?file=xxx/yyy/zz&key=value&key2=value2
-                            if(strncmp(filename,"append",6) == 0)
-                            {
+                            if (strncmp(filename,"append",6) == 0) {
                                 ACE::write_n(ACE_STDOUT, "append request", 14);
                                 char* keyvalue = strchr(filename,'?');
+                                
                                 // skep ? mark
                                 keyvalue = keyvalue + 1;
                                 ACE_HANDLE handle = ACE_INVALID_HANDLE;
-                                for(;;) {
+                                for (;;) {
                                     char* key = keyvalue;
                                     char* value = strchr(keyvalue,'=');
                                     *value = '\0';
                                     value = value + 1;
-                                    if(strchr(value,'&') == NULL) {
+                                    if (strchr(value,'&') == NULL) {
                                         // Is this key value last one?
                                         keyvalue = NULL;
 
@@ -207,101 +288,113 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[]) {
                                         // Can we stat the file?
                                         if (ACE_OS::stat (appendfile, &stat) == -1) {
                                             ACE::write_n(ACE_STDOUT,"stat fail",9);
-                                            break;
+                                            goto file_error;
                                         }
                                         ssize_t size = stat.st_size;
                                         // Can we open the file?
                                         handle = ACE_OS::open (appendfile, O_CREAT | O_RDWR | O_APPEND);
                                         if (handle == ACE_INVALID_HANDLE) {
                                             ACE::write_n(ACE_STDOUT,"open fail",9);
-                                            break;
+                                            goto file_error;
                                         }
                                     } else {
                                         // Can we write the file?
                                         if (handle == ACE_INVALID_HANDLE) {
                                             ACE::write_n(ACE_STDOUT,"write fail",9);
-                                            break;
-                                        } 
+                                            goto file_error;
+                                        }
                                         ACE_OS::write_n (handle, value, strlen(value));
-                                        if(keyvalue != NULL) {
+                                        
+                                        if (keyvalue != NULL) {
                                             ACE_OS::write_n (handle, "\t", 1);
                                         }
+
                                         // ACE_OS::write_n (ACE_STDOUT, value, strlen(value));
                                         // ACE_OS::write_n (ACE_STDOUT, "\t", 1);                                        
                                     }
 
                                     // Is there more keyvalue?
-                                    if(keyvalue == NULL) {
+                                    if (keyvalue == NULL) {
                                         ACE_OS::write_n (handle, "\n", 1);
-                                        break;
+                                        goto file_error;
                                     }
-                                } // end append
+                                } // end for
+                            file_error:
                                 if (handle != ACE_INVALID_HANDLE) {
                                     ACE_OS::close(handle);
                                 }
                                 peer.send_n("HTTP/1.0 200 OK\n", 16);
                                 ACE::write_n(ACE_STDOUT,"HTTP/1.0 200 OK\n", 16);
+                            } // end if 
                         
-                            }
-                            break;
-                        }
+                            goto peer_error;
+                        } // end if it is a query
+
                         // It is not a query
-                        if(strlen(filename)==0) {
-                            ACE::write_n(ACE_STDOUT,"filename length error",21);
-                            break;
+                        if (strlen(filename)==0) {
+                            // ACE::write_n(ACE_STDOUT,"filename length error",21);
+                            // goto peer_error;
+                            filename = const_cast<char*>("index.html");
                         }
-                        
                         ACE_stat stat;
                         // Can we stat the file?
                         if (ACE_OS::stat (filename, &stat) == -1) {
                             ACE::write_n(ACE_STDOUT,"stat fail",9);
-                            break;
+                            peer.send_n("\n", 1);
+                            ACE::write_n(ACE_STDOUT,"\n", 1);
+                            goto peer_error;
                         }
                         ssize_t size = stat.st_size;
                         if (size == 0) {
                             ACE::write_n(ACE_STDOUT,"file size error",15);
-                            break;
+                            goto peer_error;
                         }
                         // Can we open the file?
-                        ACE_HANDLE handle = ACE_OS::open (filename, O_RDONLY);
+                        ACE_HANDLE handle = ACE_OS::open (filename, O_RDONLY | O_DIRECT);
                         if (handle == ACE_INVALID_HANDLE) {
                             ACE::write_n(ACE_STDOUT,"open fail",9);
-                            break;
+                            goto peer_error;
                         }
-                        char* file = new char[size];
-                        auto_ptr<char> auto_file (file);
-                        ACE_OS::read_n (handle, file, size);
-                        ACE::write_n(ACE_STDOUT,(const char *)file,size);
+                       
+                        int ret = posix_fadvise(handle,0,0, POSIX_FADV_SEQUENTIAL);
+                        void* mm_file = mmap(NULL, size, PROT_READ, MAP_SHARED, handle, 0);
+
                         peer.send_n("HTTP/1.0 200 OK\n", 16);
                         ACE::write_n(ACE_STDOUT,"HTTP/1.0 200 OK\n", 16);
-                        char* m_mimeType = getMimeType(filename);
+                        const char* m_mimeType = getMimeType(filename);
                         peer.send_n("Content-type: ", 14);
                         peer.send_n(m_mimeType, strlen(m_mimeType));
                         peer.send_n("\n", 1);
+                        
                         ACE::write_n(ACE_STDOUT, "Content-type: ", 14);
                         ACE::write_n(ACE_STDOUT, m_mimeType, strlen(m_mimeType));
                         ACE::write_n(ACE_STDOUT, "\n", 1);
                         char* content_len = new char[16];
-                        auto_ptr<char> auto_content_len (content_len);
+                        std::unique_ptr<char> auto_content_len (content_len);
                         ACE_OS::itoa (size, content_len, 10);
                         peer.send_n("Content-length: ", 16);
                         peer.send_n(content_len, strlen(content_len));
                         peer.send_n("\n\n", 2);
-                        peer.send_n(file, size);
+                        peer.send_n(mm_file, size);
                         ACE::write_n(ACE_STDOUT, "Content-length: ", 16);
                         ACE::write_n(ACE_STDOUT, content_len, strlen(content_len));
                         ACE::write_n(ACE_STDOUT, "\n\n", 2);
-                        ACE::write_n(ACE_STDOUT, file, size);
+                        // ACE::write_n(ACE_STDOUT, mm_file, size);
+                        ret = munmap(mm_file, size);
+                        goto peer_error;
                     }
-                    break;
-                }//end if get request
-            }//end if get data
+                } //end 
+            } //end if recv result > 0
             else
             {
-                break;
+                goto peer_error;
             }
-        }//end for read data
+        } //end for read data
+    peer_error:
         peer.close();
     }//end for listen
+    mtrace();
+acceptor_error:
     return acceptor.close()==-1?1:0;
 }
+#endif // BOOST_AUTO_TEST_MAIN
